021abcaecdeca90085219f71a14a438554dc4855
Daniel Shapero <shapero.daniel@gmail.com>
2020-08-01 10:41:23 -0700

    Use vector to define partitions
    fin is a Pi type and the mathlib documentation says that vector is
    isomorphic to fin if the range has constant type; we can construct a
    vector from a value of type fin (n + 2) -> R, but then we also get all
    the machinery for sorting lists too, which will be handy for merging
    partitions

--- src/integration.lean
+++ src/integration.lean
@@ -1,15 +1,19 @@
 import analysis.calculus.deriv
 import analysis.calculus.mean_value
 
+open vector
+
 noncomputable theory
 
 variables {a : ℝ} {b : ℝ}
 
 structure partition (a : ℝ) (b : ℝ) :=
 (n : ℕ)
-(x : fin (n + 1) → ℝ)
-(inside : ∀ i : fin (n + 1), x i ∈ [a, b])
-(monotone : ∀ i j : fin (n + 1), i ≤ j → x i ≤ x j)
+(x : vector ℝ (n + 2))
+(inside : ∀ i : fin (n + 2), nth x i ∈ [a, b])
+(sorted : list.sorted (λ x y : ℝ, x ≤ y) x.val)
 
 def is_refinement (p1 : partition a b) (p2 : partition a b) :=
-∀ i : fin (p1.n + 1), ∃ j : fin (p2.n + 1), p2.x j = p1.x i
+∀ i : fin (p1.n + 2), ∃ j : fin (p2.n + 2), nth p2.x j = nth p1.x i
+
+local infix `≤`:50 := λ p1 p2 : partition a b, is_refinement p1 p2
